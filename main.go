73289ba32e10963bbaf1c98b3b50c70de37bc105
JustAHippo <82006314+JustAHippo@users.noreply.github.com>
2021-10-18 20:34:41 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Define Registers!
+	var _6 int
+  var AF int
+  var BC int
+  var DE int
+  var HL int
+	var PC int
+	var SP int
+  //Here I would read a file and handle the opcode! Too bad it isn't done yet...
+}
