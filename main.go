c559eafed0a3e714b2f50ae086e3a932d55c466b
JustAHippo <82006314+JustAHippo@users.noreply.github.com>
2021-10-18 20:38:54 -0400

    Update main.go

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	//Define Registers!
-	var _6 int
+  //Define Registers!
+  var _6 int
   var AF int
   var BC int
   var DE int
   var HL int
-	var PC int
-	var SP int
+  var PC int
+  var SP int
+  fmt.Println("Registers defined!")
   //Here I would read a file and handle the opcode! Too bad it isn't done yet...
 }
