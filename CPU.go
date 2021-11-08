c7edb164688cbd3b6a137640942f1b3d59bda835
JustAHippo <82006314+JustAHippo@users.noreply.github.com>
2021-11-07 22:14:05 -0500

    Fix comment

--- CPU.go
+++ CPU.go
@@ -32,12 +32,12 @@ func execInstr(rom []string, instr string, normalizedPointer string, orig string
 		a2 := ReadOne(rom, addrNextNext)
 		// we read it in reverse bcs of little endian (I think)
 		return a2 + a1
-	case "10": // STOP(display)
-		//This would likely terminate the display until requsted to return, however:
-		// 1. We have no implementation for display yet
-		// 2. We can't read sprite data or context that might make this not execute, but rather be sprite data.
+	case "10": // STOP(Whatever is listed in IE Register)
+		//This would likely terminate whatever is specified in the IE Register
+		// 1. We have no implementation yet
+		// 2. We can't read sprite data or context that might make this not execute, but rather be sprite data or other usage.
 		// So for the time being, we'll simply print the fact that it should have happened
-		fmt.Println("Display should possibly have been stopped.")
+		fmt.Println("Something should possibly have been stopped.")
 
 	default:
 		fmt.Println("Unknown inst: " + instr)
