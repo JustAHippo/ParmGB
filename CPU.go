c544da8c48e493cf7491546d164930afb2c622b3
JustAHippo <82006314+JustAHippo@users.noreply.github.com>
2021-11-07 22:00:57 -0500

    "Handle" STOP Display(0x10)

--- CPU.go
+++ CPU.go
@@ -32,6 +32,12 @@ func execInstr(rom []string, instr string, normalizedPointer string, orig string
 		a2 := ReadOne(rom, addrNextNext)
 		// we read it in reverse bcs of little endian (I think)
 		return a2 + a1
+	case "10": // STOP(display)
+		//This would likely terminate the display until requsted to return, however:
+		// 1. We have no implementation for display yet
+		// 2. We can't read sprite data or context that might make this not execute, but rather be sprite data.
+		// So for the time being, we'll simply print the fact that it should have happened
+		fmt.Println("Display should possibly have been stopped.")
 
 	default:
 		fmt.Println("Unknown inst: " + instr)
